b9e2e8c049eb609c6b30f6faa51a68d4a3905ff8
Keanu Amadius Gonza <140991645+keanugonza@users.noreply.github.com>
2024-05-04 08:37:17 +0700

    Feat: create all algorithm

---
+++ src/UCS.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+public class UCS{
+
+    public static List<String> UCS_Algorithm(String start, String end, List<String> Database) {
+        if (!Database.contains(start) || !Database.contains(end)) {
+            return null;
+        }
+
+        PriorityQueue<Node> queue = new PriorityQueue<>();
+        queue.add(new Node(start, 0, null));
+
+        List<String> visited = new ArrayList<>();
+        visited.add(start);
+
+        int count = 1;
+        while (!queue.isEmpty()) {
+            Node currentNode = queue.poll();
+            String currentWord = currentNode.word;
+            visited.add(currentWord);
+
+            if (currentWord.equals(end)) {
+                List<String> result = new ArrayList<>();
+                result = makePath(currentNode);
+                result.add(0, String.valueOf(count));
+                return result;
+            }
+
+            List<String> neighbors = Util.getNeighboors(currentWord, Database);
+            count++;
+
+            for (String neighbor : neighbors) {
+                if (!visited.contains(neighbor)) {
+                    visited.add(neighbor);
+                    Node newNode = new Node(neighbor, currentNode.cost + 1, currentNode);
+                    queue.add(newNode);
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private static List<String> makePath(Node node) {
+        List<String> result = new ArrayList<>();
+        while (node != null) {
+            result.add(node.word);
+            node = node.path;
+        }
+        Collections.reverse(result);
+        return result;
+    }
+
+//    public static void printPriorityQueue(PriorityQueue<Node> queue) {
+//        System.out.print("Priority Queue: ");
+//
+//        Iterator<Node> iterator = queue.iterator();
+//
+//        // Iterasi melalui PriorityQueue dan cetak nama beserta cost
+//        while (iterator.hasNext()) {
+//            Node node = iterator.next();
+//            System.out.print(node.word + " (Cost: " + node.cost + ")");
+//
+//            if (iterator.hasNext()) {
+//                System.out.print(", "); // Tambahkan koma jika masih ada elemen
+//            }
+//        }
+//
+//        System.out.println(); // Pindah baris setelah mencetak queue
+//    }
+}
---
+++ src/Util.java
@@ -0,0 +1,75 @@
+import java.io.*;
+import java.util.*;
+public class Util {
+    public static List<String> readWordsFromFile() {
+        String filename = "src\\words_alpha.txt";
+        List<String> words = new ArrayList<>();
+
+        System.out.println("Current working directory: " + System.getProperty("user.dir")); // debug info
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] lineWords = line.split("\\s+");
+                words.addAll(Arrays.asList(lineWords));
+            }
+        } catch (IOException e) {
+            System.err.println("Error membaca file: " + e.getMessage());
+        }
+
+        return words;
+    }
+
+    public static List<String> getNeighboors(String FindWord, List<String> Database){
+        List<String> neighboors = new ArrayList<>();
+        for(String Word : Database){
+            int beda = 0;
+            for(int i = 0; i < Word.length(); i++){
+                if (Word.charAt(i) != FindWord.charAt(i)){
+                    beda++;
+                }
+            }
+            if(beda == 1){
+                neighboors.add(Word);
+            }
+
+        }
+        return neighboors;
+    }
+
+    public static void printAlgo(){
+        System.out.println("\nAlgoritma yang bisa digunakan: ");
+        System.out.println("0. Semua algoritma (cheat)");
+        System.out.println("1. UCS");
+        System.out.println("2. GBFS");
+        System.out.println("3. A*");
+        System.out.print("Pilih nomor algoritma: ");
+    }
+
+    public static void printHasil(List<String> Result){
+        if (Result != null){
+            for(int i = 0; i < Result.size(); i++){
+                if(i==0){
+                    System.out.println("\n======================JAWABAN======================");
+                    System.out.println("Jumlah Node dikunjungi: " + Result.get(i));
+                    System.out.print("Path: ");
+                } else if (i != Result.size()-1) {
+                    System.out.print(Result.get(i) + " -> ");
+                } else{
+                    System.out.print(Result.get(i));
+                }
+            }
+        }else{
+            System.out.println("jalur tidak ditemukan");
+        }
+
+    }
+
+    public static boolean isExist(String word, List<String> Database){
+        if (Database.contains(word)){
+            return true;
+        } else{
+            return false;
+        }
+    }
+}
